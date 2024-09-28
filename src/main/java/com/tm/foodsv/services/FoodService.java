ca0a7c9bbb7898b761814a0668f5552f19c69183
IrvinTM <tm22012@gmail.com>
2024-09-27 22:51:30 -0600

    service sodium

--- src/main/java/com/tm/foodsv/services/FoodService.java
+++ src/main/java/com/tm/foodsv/services/FoodService.java
@@ -3,22 +3,13 @@ package com.tm.foodsv.services;
 import com.tm.foodsv.entities.Category;
 import com.tm.foodsv.entities.Food;
 import com.tm.foodsv.entities.NovaClasification;
-<<<<<<< HEAD
-=======
 import com.tm.foodsv.entities.Warnings;
->>>>>>> 0e35d7c (sodium)
 import com.tm.foodsv.repositories.FoodRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class FoodService {
-<<<<<<< HEAD
-    private final FoodRepository foodRepository;
-
-    public FoodService(FoodRepository foodRepository) {
-        this.foodRepository = foodRepository;
-=======
     private final double MAX_SODIUM_PER_100G = 320;
     private final double MAX_SODIUM_PER_BEVERAGE_NO_CALORIES = 45;
 
@@ -28,7 +19,6 @@ public class FoodService {
     public FoodService(FoodRepository foodRepository, Warnings warnings) {
         this.foodRepository = foodRepository;
         this.warnings = warnings;
->>>>>>> 0e35d7c (sodium)
     }
 
     public List<Food> getAllFoods() {
@@ -36,14 +26,11 @@ public class FoodService {
     }
 
     public void addFood(Food food) {
-<<<<<<< HEAD
-=======
         if(food.getCategory() == Category.BEVERAGES){
             if(food.getCalories() == 0 && food.getSodium() > MAX_SODIUM_PER_BEVERAGE_NO_CALORIES){
                 food.getWarnings().concat(warnings.HIGH_SODIUM.toString());
             }
         }
->>>>>>> 0e35d7c (sodium)
         foodRepository.save(food);
     }
 
